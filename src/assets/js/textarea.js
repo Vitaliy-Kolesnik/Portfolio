bdca6adf59089830f64f79c1f843d3f60b05076d
Vitaliy-Kolesnik <vitaliym58@gmail.com>
2022-03-27 22:01:31 +0300

    delete src

--- src/assets/js/textarea.js
+++
@@ -1,9 +0,0 @@
-const textArea = document.querySelectorAll('[data-autoresize]');
-textArea.forEach(item =>{
-   item.addEventListener('input', event =>{
-      let $this = event.target;
-
-      $this.style.height = '30px';
-      $this.style.height = $this.scrollHeight + 'px';
-   });
-});
